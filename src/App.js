91dc52959a57cff37904646bd9995f89f0e6f892
Rohit-24gb <rohitrajaksnd@gmail.com>
2024-01-21 15:41:56 +0530

    commit one

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
+import Homepage from './customer/pages/Homepage/Homepage';
+
+function App() {
+  return (
+   <div>
+    <Navigation/>
+    <div>
+    <Homepage/>
+    </div>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,22 @@
+export const mainCarouselData =[
+        {
+            // image : "src/assets/carousel/full-shot-woman-online-fashion-shopping_23-2150460083.avif",
+            image :"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_5.jpg",
+            path : "/full-shot-woman-online-fashion-shopping_23-2150460083.avif"
+        },
+        {
+            // image : "src/assets/carousel/online-marketing_53876-176744.avif",
+            image :"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+            path : ""
+        },
+        {
+            // image : "src/assets/carousel/showing-cart-trolley-shopping-online-sign-graphic_53876-133967.avif",
+            image :"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/_/9_8.jpg",
+            path : ""
+        },
+        {
+            // image : "src/assets/carousel/social-media-marketing-concept-marketing-with-applications_23-2150063168.avif",
+            image :"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_4.jpg",
+            path : ""
+        }
+]
